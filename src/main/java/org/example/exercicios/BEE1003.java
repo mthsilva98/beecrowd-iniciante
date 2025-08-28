bdb403cd543bb7ba7687bdd4ee0c65405814dcbc
mthsilva98 <mthsilva1998@gmail.com>
2025-08-28 15:20:52 -0300

    BEE1003 exercise

---
+++ src/main/java/org/example/exercicios/BEE1003.java
@@ -0,0 +1,19 @@
+package org.example.exercicios;
+
+import java.util.Scanner;
+
+public class BEE1003 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a, b;
+
+        a = sc.nextInt();
+        b = sc.nextInt();
+
+        int soma = a + b;
+
+        System.out.println("SOMA = " + soma);
+
+    }
+}
