2b7a43799f714206cf3625efdc99de8d901db736
mthsilva98 <mthsilva1998@gmail.com>
2025-08-30 12:29:30 -0300

    BEE1010 exercise

---
+++ src/main/java/org/example/exercicios/BEE1010.java
@@ -0,0 +1,34 @@
+package org.example.exercicios;
+
+import java.util.Scanner;
+
+public class BEE1010 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int codigoPeca1;
+        int quantidadePeca1;
+        double valorPeca1;
+
+        int codigoPeca2;
+        int quantidadePeca2;
+        double valorPeca2;
+
+        codigoPeca1 = sc.nextInt();
+        quantidadePeca1 = sc.nextInt();
+        valorPeca1 = sc.nextDouble();
+
+        codigoPeca2 = sc.nextInt();
+        quantidadePeca2 = sc.nextInt();
+        valorPeca2 = sc.nextDouble();
+
+        double valorTolta = (valorPeca1 * quantidadePeca1) + (valorPeca2 * quantidadePeca2);
+
+        String format = String.format("%.2f", valorTolta);
+
+        System.out.println("VALOR A PAGAR: R$ " + format);
+
+        sc.close();
+
+    }
+}
