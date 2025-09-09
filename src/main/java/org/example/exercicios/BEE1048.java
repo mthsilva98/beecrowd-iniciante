82ed3ddda64034619dfecfcaaaec9859f1f90583
mthsilva98 <mthsilva1998@gmail.com>
2025-09-08 23:45:33 -0300

    BEE1048 exercise

---
+++ src/main/java/org/example/exercicios/BEE1048.java
@@ -0,0 +1,35 @@
+package org.example.exercicios;
+
+import java.util.Scanner;
+
+public class BEE1048 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double salario = sc.nextDouble();
+        int percentual;
+
+        if (salario <= 400.00) {
+            percentual = 15;
+        } else if (salario <= 800.00) {
+            percentual = 12;
+        } else if (salario <= 1200.00) {
+            percentual = 10;
+        } else if (salario <= 2000.00) {
+            percentual = 7;
+        } else {
+            percentual = 4;
+        }
+
+
+        double reajusteGanho = salario * (percentual / 100.0);
+        double novoSalario = salario + reajusteGanho;
+
+        System.out.printf("Novo salario: %.2f\n", novoSalario);
+        System.out.printf("Reajuste ganho: %.2f\n", reajusteGanho);
+        System.out.println("Em percentual: " + percentual + " %");
+
+        sc.close();
+
+    }
+}
